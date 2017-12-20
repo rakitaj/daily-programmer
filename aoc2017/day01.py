b34839d1f5729f51b0f80fc973c2f61d3ff7cd95
Joshua Rakita <rakitaj@users.noreply.github.com>
2017-12-20 14:48:26 -0800

    Implement IterableNumber

--- aoc2017/day01.py
+++ aoc2017/day01.py
@@ -7,13 +7,15 @@ class IterableNumber(object):
         self.position = 0
         self.cycled = False
 
-    def next(self) -> int:
+    def current(self) -> int:
+        return int(self.value[self.position])
+
+    def advance(self):
         if self.position + 1 == len(self.value):
             self.cycled = True
-            return int(self.value[self.position])
+            self.position = 0
         else:
             self.position += 1
-            return int(self.value[self.position])
 
     def reset(self):
         self.position = 0
@@ -21,16 +23,15 @@ class IterableNumber(object):
 
 def inverse_captcha(input_number: int) -> int:
     sum = 0
-    string_number = str(input_number)
-    for i in range(0, len(string_number)):
-        if i + 1 == len(string_number):
-            if string_number[i] == string_number[0]:
-                sum += int(string_number[i])
-        else:
-            if string_number[i] == string_number[i + 1]:
-                sum += int(string_number[i])
+    number = IterableNumber(input_number)
+    while number.cycled is False:
+        current = number.current()
+        number.advance()
+        next = number.current()
+        if current == next:
+            sum += number.current()
     return sum
 
 if __name__ == "__main__":
-    number = common.number_from_text_file("day01_input.txt")
-    print(inverse_captcha(number))
+    challenge_input = common.number_from_text_file("day01_input.txt")
+    print(inverse_captcha(challenge_input))
