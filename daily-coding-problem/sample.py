5fef70aee77051101640d81f4311ae82b555f0b4
Joshua Rakita <rakitaj@users.noreply.github.com>
2018-02-21 17:34:18 -0800

    Update .gitignore for pytest cache and start new problem type.

---
+++ daily-coding-problem/sample.py
@@ -0,0 +1,7 @@
+from typing import Tuple
+
+def climb_staircase(n: int, climb_set: Tuple[int, ...]) -> int:
+    return 5
+
+def test_climb_staircase_with_provider_answer():
+    assert 5 == climb_staircase(4, (1, 2))
