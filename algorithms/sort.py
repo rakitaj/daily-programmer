9411b74a3dcd6aef4c5c92a2604b9e37d13412ad
Joshua Rakita <rakitaj@users.noreply.github.com>
2018-07-25 20:18:45 -0700

    Finished selection and insertion sort algos.

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -19,7 +19,23 @@ def bubble_sort(elements: List) -> List:
 def insertion_sort(elements: List) -> List:
     length = len(elements)
     for i in range(1, length):
-        for j in range(i, -1, -1):
-            if elements[i] > elements[j]:
-                in_place_swap(elements, i, j)
+        for j in range(i, 0, -1):
+            if elements[j-1] > elements[j]:
+                in_place_swap(elements, j, j-1)
     return elements
+
+def selection_sort(elements: List) -> List:
+    length = len(elements)
+    for i in range(0, length):
+        j = find_min_element_index(elements, i)
+        in_place_swap(elements, i, j)
+    return elements
+
+def find_min_element_index(elements: List, start: int) -> int:
+    min_index = start
+    min_value = elements[start]
+    for i in range(start, len(elements)):
+        if elements[i] < min_value:
+            min_value = elements[i]
+            min_index = i
+    return min_index
--- algorithms/test_sort.py
+++ algorithms/test_sort.py
@@ -18,3 +18,23 @@ def test_bubble_sort(unsorted, expected):
 ])
 def test_insertion_sort(unsorted, expected):
     assert sort.insertion_sort(unsorted) == expected
+
+@pytest.mark.parametrize("unsorted, expected", [
+    ([0, 3, 3, 2, 1, 0], [0, 0, 1, 2, 3, 3]),
+    ([5, -3, -2, 12, 123, 2], [-3, -2, 2, 5, 12, 123])
+])
+def test_selection_sort(unsorted, expected):
+    assert sort.selection_sort(unsorted) == expected
+
+@pytest.mark.parametrize("elements, start, expected", [
+    ([1, 2, 3], 0, 0),
+    ([1, 2, 3], 1, 1),
+    ([1, 2, 3], 2, 2),
+    ([8, 2, 4, 2, 6], 0, 1),
+    ([8, 2, 4, 2, 6], 1, 1),
+    ([8, 2, 4, 2, 6], 2, 3),
+    ([8, 2, 4, 2, 6], 3, 3),
+    ([8, 2, 4, 2, 6], 4, 4)
+])
+def test_find_minimum_element_index(elements, start, expected):
+    assert sort.find_min_element_index(elements, start) == expected
