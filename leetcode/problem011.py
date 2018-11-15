a9b98fd7005bde21bc2f0f0b31852ea245c9987b
Joshua Rakita <rakitaj@users.noreply.github.com>
2018-11-14 16:48:43 -0800

    Linear algorithm for Leetcode problem 11.

--- leetcode/problem011.py
+++ leetcode/problem011.py
@@ -1,15 +1,33 @@
 from typing import List
 
-def container_most_water(numbers: List[int]) -> int:
-    """Find the number combo which gives the container with the most water."""
-    max_area = 0
-    for i in range(len(numbers)):
-        for j in range(i, len(numbers)):
-            area = calculate_area(numbers[i], numbers[j], j - i)
+class Problem011:
+
+    def container_most_water(self, numbers: List[int]) -> int:
+        """Find the number combo which gives the container with the most water."""
+        max_area = 0
+        for i in range(len(numbers)):
+            for j in range(i, len(numbers)):
+                area = self.calculate_area(numbers[i], numbers[j], j - i)
+                if area > max_area:
+                    max_area = area
+        return max_area
+
+    def container_most_water_linear(self, numbers: List[int]) -> int:
+        """Find the number combo which gives the container with the most water
+        use a linear algorithm that contracts from both sides."""
+        max_area = 0
+        start = 0
+        end = len(numbers) - 1
+        while start != end:
+            area = self.calculate_area(numbers[start], numbers[end], end - start)
             if area > max_area:
                 max_area = area
-    return max_area
+            if numbers[start] <= numbers[end]:
+                start += 1
+            else:
+                end -= 1
+        return max_area
 
-def calculate_area(height1, height2, distance):
-    lower_bound = min(height1, height2)
-    return lower_bound * distance
+    def calculate_area(self, height1, height2, distance):
+        lower_bound = min(height1, height2)
+        return lower_bound * distance
--- leetcode/test_problem011.py
+++ leetcode/test_problem011.py
@@ -10,7 +10,18 @@ class TestProblem(object):
         ([1, 2, 4, 2, 0, 4, 3], 12)
     ])
     def test_container_most_water(self, test_data, expected):
-        assert container_most_water(test_data) == expected
+        assert Problem011().container_most_water(test_data) == expected
+
+    @pytest.mark.parametrize("test_data, expected", [
+        ([5, 3], 3),
+        ([5, 2, 5], 10),
+        ([1, 2, 4, 2, 0, 4], 12),
+        ([1, 2, 4, 2, 0, 4, 3], 12),
+        ([1,8,6,2,5,4,8,3,7], 49)
+        ])
+    def test_container_most_water_linear(self, test_data, expected):
+        assert Problem011().container_most_water_linear(test_data) == expected
+
 
     @pytest.mark.parametrize("height1, height2, distance, expected", [
         (5, 3, 3, 9),
@@ -19,4 +30,4 @@ class TestProblem(object):
         (9, 6, 2, 12)
     ])
     def test_calculate_area(self, height1, height2, distance, expected):
-        assert calculate_area(height1, height2, distance) == expected
+        assert Problem011().calculate_area(height1, height2, distance) == expected
