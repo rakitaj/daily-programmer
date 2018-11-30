012a7d9d5f2c7eb93545e44627ada5fca6a2abab
Joshua Rakita <rakitaj@users.noreply.github.com>
2018-11-29 17:27:10 -0800

    Problem023 some tests passing. Need to do core algo.

---
+++ leetcode/problem023.py
@@ -0,0 +1,41 @@
+from typing import List, Tuple
+
+
+class ListNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.next: ListNode = None
+
+
+class Solution:
+
+    def merge_lists(self, lists: List[ListNode]) -> ListNode:
+        pass
+
+    @staticmethod
+    def find_min_index(lists: List[ListNode]) -> int:
+        length = len(lists)
+        if length == 0:
+            return -1
+        elif length == 1:
+            return 0
+        else:
+            min_index = 0
+            for i in range(length):
+                if lists[i].val < lists[min_index].val:
+                    min_index = i
+            return min_index
+
+    @staticmethod
+    def min_and_dequeue(lists: List[ListNode]) -> Tuple[ListNode, List[ListNode]]:
+        min_index = Solution.find_min_index(lists)
+        min_node = lists[min_index]
+        lists[min_index] = lists[min_index].next
+        return (min_node, lists)
+
+    @staticmethod
+    def all_none(lists) -> bool:
+        return all(x is None for x in lists)
+
+    mergeKLists = merge_lists
---
+++ leetcode/test_problem023.py
@@ -0,0 +1,29 @@
+from typing import List
+from .problem023 import Solution, ListNode
+
+
+def test_min_of_lists_flat_3():
+    lists: List[ListNode] = [ListNode(12), ListNode(3), ListNode(15)]
+    min_index = Solution.find_min_index(lists)
+    assert min_index == 1
+
+
+def test_lists_all_none_should_be_true_when_every_list_element_is_none():
+    lists = [None, None, None, None, None]
+    assert Solution.all_none(lists) is True
+
+
+def test_lists_all_none_should_be_false_when_any_element_is_not_none():
+    lists = [None, None, None, ListNode(50000)]
+    assert Solution.all_none(lists) is False
+
+
+def test_min_and_remove():
+    ln_1 = ListNode(1)
+    ln_2 = ListNode(2)
+    ln_3 = ListNode(3)
+    ln_1.next = ln_3
+    lists: List[ListNode] = [ln_1, ln_2]
+    result = Solution.min_and_dequeue(lists)
+    assert result[0].val == ln_1.val
+    assert result[1] == [ln_3, ln_2]
