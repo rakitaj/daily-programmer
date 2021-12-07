ad0350b362d4353ea1ee1f046e736ed9880cdf4c
Joshua Rakita <joshuarakita@gmail.com>
2021-12-06 22:44:13 -0800

    Fixup paths so they work from anywhere.

--- aoc2021/input_helpers.py
+++ aoc2021/input_helpers.py
@@ -1,6 +1,11 @@
+from os.path import abspath, dirname
+
+
 def puzzle_input_to_str(day: int, strip: bool = False) -> list[str]:
     day_number = str(day).zfill(2)
-    with open(f"./puzzleinput/day{day_number}.txt", encoding="utf8") as f:
+    dir_name = dirname(__file__)
+    file_path = abspath(f"{dir_name}/puzzleinput/day{day_number}.txt")
+    with open(file_path, encoding="utf8") as f:
         lines = f.readlines()
     if strip:
         lines = [line.strip() for line in lines]
@@ -9,7 +14,9 @@ def puzzle_input_to_str(day: int, strip: bool = False) -> list[str]:
 
 def puzzle_input_to_ints(day: int) -> list[int]:
     day_number = str(day).zfill(2)
-    with open(f"./puzzleinput/day{day_number}.txt", encoding="utf8") as f:
+    dir_name = dirname(__file__)
+    file_path = abspath(f"{dir_name}/puzzleinput/day{day_number}.txt")
+    with open(file_path, encoding="utf8") as f:
         lines = f.readlines()
     lines_as_ints = [int(line) for line in lines]
     return lines_as_ints
